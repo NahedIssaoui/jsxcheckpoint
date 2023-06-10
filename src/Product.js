60ee0d4b717fc5015ccd0b3f97a1209c5d170883
NahedIssaoui <issaouinahed96@gmail.com>
2023-06-10 14:03:47 +0100

    first jsx commit 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/Description.js 	new file:   src/Image.js 	new file:   src/Name.js 	new file:   src/Price.js 	new file:   src/Product.js 	modified:   src/index.js 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/Product.js
@@ -0,0 +1,11 @@
+const product = {
+    name: "One piece manga",
+    price: 9.99 ,
+    description: "One Piece is the story of Monkey D. Luffy, a young man who has a single dream: To find the legendary treasure known as the One Piece and become the King of the Pirates.",
+    image: "https://th.bing.com/th/id/OIP.jPINN5ICJ3Z2qM9K1D4VHgHaFQ?pid=ImgDet&rs=1.jpg"
+    
+  };
+  export default product;
+
+ 
+  
